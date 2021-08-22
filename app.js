cf268968201b110aa1633aa4525336d7f4534eac
sinhyuk <rlatls1997@gmail.com>
2021-08-22 16:58:55 +0900

    refactor: root router 추가, module별 directory 분리

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mysql = require("mysql");
-
-//외부 router module import
-var main = require('./router/main')
-var email = require('./router/email')
-
-//*************************************************mysql 연결설정
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "pass4360",
-  database: "jsman",
-});
-
-connection.connect();
-//*************************************************mysql 연결설정
+//main router module import
+var router = require("./router/index");
 
 app.listen(3000, function () {
   console.log("start!! express server on port 3000");
 });
 
+//********미들웨어 사용 정의 */
 //static 파일을 요청할 떄 매번 get요청 경로를 지정해주지말고
 //app.use로 'public' 밑에 있는 정적 파일들을 모두 제공하도록 설정
 app.use(express.static("public"));
@@ -31,14 +17,5 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-//main으로 들어오면 router/main으로 처리를 하라는 말
-//router/main.js 파일의 /경로인 메소드 처리
-//*app.js에서 설정한 bodyparser나 static파일 설정은 하위 router모듈에도 적용됨 */
-app.use('/main', main);
-app.use('/email', email);
-
-app.get("/", function (req, res) {
-  //res.send("<h1>hi friend!</h1>")
-  //__dirname은 현재 dir절대경로, get요청시 main.html파일 제공
-  res.sendFile(__dirname + "/public/main.html");
-});
+// path가 '/'이면 생략가능
+app.use(router);
---
+++ router/index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+//express 내부에 정의된 Router메소드
+//api를 분리하여 routing을 모듈화할 때 사용한다.
+var router = express.Router();
+//상대경로로 쉽게 이동하기 위한 모듈
+var path = require("path");
+var main = require("./main/main");
+var email = require("./email/email");
+var form = require("./form/form");
+
+router.get("/", function (req, res) {
+  //res.send("<h1>hi friend!</h1>")
+  //__dirname은 현재 dir절대경로, get요청시 main.html파일 제공
+  res.sendFile(path.join(__dirname, "../public/main.html"));
+});
+
+//main으로 들어오면 router/main으로 처리를 하라는 말
+//router/main.js 파일의 /경로인 메소드 처리
+//*app.js에서 설정한 bodyparser나 static파일 설정은 하위 router모듈에도 적용됨 */
+router.use("/main", main);
+router.use("/email", email);
+router.use("/form", form);
+
+module.exports = router;
